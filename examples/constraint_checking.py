2aca95b0d98c0d835382406197034d70d358933e
Florian Bachinger <flo.bachinger@gmail.com>
2023-05-19 18:17:31 +0200

    fix some text in the examples

--- examples/constraint_checking.py
+++ examples/constraint_checking.py
@@ -6,6 +6,7 @@ import SCR_Benchmarks.SRSDFeynman as srsdf
 from SCR_Benchmarks.constraints import SCRBenchmark
 
 ICh6Eq20 = SCRBenchmark(srsdf.FeynmanICh6Eq20)
+# Raw: exp(-(theta / sigma) ** 2 / 2) / (sqrt(2 * pi) * sigma)
 # is monotonic increasing over theta in -inf. <= theta <= 0
 # is monotonic decreasing over theta in 0 <= theta <= inf.
 
@@ -22,7 +23,7 @@ print(ICh6Eq20.check_constraints("x0"))
 #prints false as the increasing constraint for -inf. <= theta <= 0 is violated
 print(ICh6Eq20.check_constraints("-x0"))
 
-#prints false as both constraints for x0 (theta) is violated
+#prints false as both constraints for x0 (theta) are violated
 print(ICh6Eq20.check_constraints("(x0*x0)"))
 
 ########################################################################
@@ -37,5 +38,5 @@ print(ICh6Eq20.check_constraints("theta",use_display_names=True))
 #prints false as the increasing constraint for -inf. <= theta <= 0 is violated
 print(ICh6Eq20.check_constraints("-theta",use_display_names=True))
 
-#prints false as both constraints for x0 (theta) is violated
+#prints false as both constraints for x0 (theta) are violated
 print(ICh6Eq20.check_constraints("(theta*theta)",use_display_names=True))
