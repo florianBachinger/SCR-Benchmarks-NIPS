0908f4ee68c1321e87454f752efa475d3f77d07e
Florian Bachinger <flo.bachinger@gmail.com>
2023-08-24 13:05:22 +0200

    increment version for new JAX support

--- SCRBenchmark/__init__.py
+++ SCRBenchmark/__init__.py
@@ -5,7 +5,7 @@
 """
 
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 # import ApiClient
 from SCRBenchmark import SRSDFeynman
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "SCRBenchmark"
-VERSION = "0.4.0"
+VERSION = "0.5.0"
 # To install the library, run the following
 #
 # python setup.py install
